d7ad6faad681c2fb74625e4eaa43d3d4e3c108ba
luisoliv <luisdoom1995@hotmail.com>
2017-05-18 02:13:32 -0400

    Basic 0.1 version
    +Added the basic structure of the project
    +Added the HTML template to use
    +Added the login screen to enter into Youtube and Twitter accounts

---
+++ assets/js/youtube.js
@@ -0,0 +1,20 @@
+function loginYoutube()
+{
+	//in twitter the username could be as the username or a email address
+	var user = document.getElementById("username").value;
+	var pass = document.getElementById("pass").value;
+
+	console.log("user: "+user+"\npass: "+pass);
+
+	var isUsername;
+
+	isUsername = checkTypeUser(user);
+
+}
+
+//this fucntion checks if the username is email address or actual username
+function checkTypeUser(user)
+{
+	//some regular expression will be here...
+}
+
